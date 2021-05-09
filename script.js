29ccea9625e20999e9c5abb029bac495f5722e99
laefad <FreeFlightA@yandex.ru>
2021-05-10 00:16:31 +0300

    fix mapper buttons creating

--- script.js
+++ script.js
@@ -17,87 +17,87 @@
 	
 	const tableMapper = new TableMapper(document.getElementById("container"));
 	
+	tableMapper.getTableBuilder()
+		.setName("rating")
+		.setSynonym("Рейтинг")
+		.addColumn("name", {
+			synonym: "Имя игрока",
+			otherwise: true,
+			sortable: false,
+			sNumeric: false
+		})
+		.addColumn("rating", {
+			synonym: "Рейтинг",
+			otherwise: true,
+			sortable: true,
+			isNumeric: true
+		})
+		.addTable();
+		
+	tableMapper.getTableBuilder()
+		.setName("wins")
+		.setSynonym("Победы")
+		.addColumn("name", {
+			synonym: "Имя игрока",
+			otherwise: true,
+			sortable: false,
+			isNumeric: false
+		})
+		.addColumn("count", {
+			synonym: "Игры",
+			otherwise: true,
+			sortable: true,
+			isNumeric: true
+		})
+		.addColumn("total_wins", {
+			synonym: "Победы",
+			otherwise: true,
+			sortable: true,
+			isNumeric: true
+		})
+		.addColumn("team_wins", {
+			synonym: "Командные",
+			otherwise: true,
+			sortable: true,
+			isNumeric: true
+		})
+		.addColumn("personal_wins", {
+			synonym: "Одиночные",
+			otherwise: true,
+			sortable: true,
+			isNumeric: true
+		})
+		.addColumn("win_rate", {
+			synonym: "Win rate",
+			otherwise: true,
+			sortable: true,
+			isNumeric: true
+		})
+		.addTable();
+	
+	// tableMapper.getTableBuilder()
+	// .setName("other")
+	// .setSynonym("Остальное")
+	// .addColumn("name", {
+		// synonym: "Имя игрока",
+		// otherwise: true,
+		// sortable: false,
+		// isNumeric: false
+	// })
+	// .addColumn("highest_score_take", {
+		// synonym: "Наибольшая прибавка рейтинга",
+		// otherwise: true,
+		// sortable: true,
+		// isNumeric: true
+	// })
+	// .addColumn("changes", {
+		// synonym: "Изменения рейтинга"
+	// })
+	// .addTable();
+	
 	const onValueUpdate = (data) => {
 		
 		tableMapper.clear();
-		
-		tableMapper.getTableBuilder()
-			.setName("rating")
-			.setSynonym("Рейтинг")
-			.addColumn("name", {
-				synonym: "Имя игрока",
-				otherwise: true,
-				sortable: false,
-				sNumeric: false
-			})
-			.addColumn("rating", {
-				synonym: "Рейтинг",
-				otherwise: true,
-				sortable: true,
-				isNumeric: true
-			})
-			.addTable();
-			
-		tableMapper.getTableBuilder()
-			.setName("wins")
-			.setSynonym("Победы")
-			.addColumn("name", {
-				synonym: "Имя игрока",
-				otherwise: true,
-				sortable: false,
-				isNumeric: false
-			})
-			.addColumn("count", {
-				synonym: "Игры",
-				otherwise: true,
-				sortable: true,
-				isNumeric: true
-			})
-			.addColumn("total_wins", {
-				synonym: "Победы",
-				otherwise: true,
-				sortable: true,
-				isNumeric: true
-			})
-			.addColumn("team_wins", {
-				synonym: "Командные",
-				otherwise: true,
-				sortable: true,
-				isNumeric: true
-			})
-			.addColumn("personal_wins", {
-				synonym: "Одиночные",
-				otherwise: true,
-				sortable: true,
-				isNumeric: true
-			})
-			.addColumn("win_rate", {
-				synonym: "Win rate",
-				otherwise: true,
-				sortable: true,
-				isNumeric: true
-			})
-			.addTable();
-			
-		// tableMapper.getTableBuilder()
-			// .setName("other")
-			// .setSynonym("Остальное")
-			// .addColumn("name", {
-				// synonym: "Имя игрока",
-				// otherwise: true,
-				// sortable: false,
-				// isNumeric: false
-			// })
-			// .addColumn("highest_score_take", {
-				// synonym: "Наибольшая прибавка рейтинга",
-				// otherwise: true,
-				// sortable: true,
-				// isNumeric: true
-			// })
-			// .addColumn("changes", {
-				// synonym: "Изменения рейтинга"
-			// })
-			// .addTable();
 		
 		data.forEach(
 			user => tableMapper.addRow(user)
