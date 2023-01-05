7417335a172e09dc0162bbe86bce15187d08a937
laefad <FreeFlightA@yandex.ru>
2023-01-06 02:56:13 +0300

    rework site to nuxt framework

---
+++ composables/useFirebaseValueFromPath.ts
@@ -0,0 +1,29 @@
+// Utilities
+import { watch, ref } from 'vue'
+import { ref as firebaseRef, onValue, set } from 'firebase/database'
+
+// https://firebase.google.com/docs/database/web/read-and-write#write_data
+// https://v3.nuxtjs.org/guide/directory-structure/composables
+export const useFirebaseValueFromPath = <T>(
+    path: string
+) => {
+    const { $firebase } = useNuxtApp()
+    const query = firebaseRef($firebase, path)
+
+    // Я так и не понял, оно кеширует или нет, если нет, то можно прям тут кешировать для первичного получения данных
+    const value = ref<T>()
+
+    // Получаем обновления данных из firebase
+    onValue(query, (snapshot) => {
+        const data: T = snapshot.val()
+        value.value = data
+    })
+
+    // Ключ все равно только на чтение, но может когда-нибудь можно будет использовать для админки
+    // // Отправляем обновления данных в firebase
+    // watch(value, () => {
+    //     set(query, value.value)
+    // })
+
+    return value
+}
---
+++ pages/details.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="column">
+    <h2>
+      Детальный рейтинг игроков
+    </h2>
+    <DataTable :data="data" :columns="columns"/>
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+const data = computed(() => useFirebaseValueFromPath<any>('/table/players/').value ?? []);
+const columns = ['name', 'rating', 'count', 'total_wins', 'personal_wins', 'team_wins', 'win_rate']
+
+</script>
