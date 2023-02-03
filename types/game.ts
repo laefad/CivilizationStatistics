05648bdf77cf607169153bfce0133c102303a520
laefad <FreeFlightA@yandex.ru>
2023-02-03 14:35:34 +0300

    update game type

--- types/game.ts
+++ types/game.ts
@@ -5,4 +5,7 @@ export type Game = {
     teams: Team[]
     turns: number
     end_reason: string
+    start_date: string     // YYYY-MM-DD
+    finish_date: string    // YYYY-MM-DD
+    seconds_per_move: null | number
 }
