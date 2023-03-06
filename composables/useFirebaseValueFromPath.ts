98a16893373c1a6ea1415f2f5125b111fb0b68e8
laefad <FreeFlightA@yandex.ru>
2023-03-06 16:33:26 +0300

    fix type error and add docs to useFirebaseValue

--- composables/useFirebaseValueFromPath.ts
+++ composables/useFirebaseValueFromPath.ts
@@ -1,9 +1,11 @@
+// Types
+import type { UnwrapRef } from 'vue'
+
 // Utilities
-import { watch, ref } from 'vue'
-import { ref as firebaseRef, onValue, set } from 'firebase/database'
+import { ref } from 'vue'
+import { ref as firebaseRef, onValue } from 'firebase/database'
 
 // https://firebase.google.com/docs/database/web/read-and-write#write_data
-// https://v3.nuxtjs.org/guide/directory-structure/composables
 export const useFirebaseValueFromPath = <T>(
     path: string,
     defaultValue: T
@@ -11,17 +13,16 @@ export const useFirebaseValueFromPath = <T>(
     const { $firebase } = useNuxtApp()
     const query = firebaseRef($firebase, path)
 
-    // Я так и не понял, оно кеширует или нет, если нет, то можно прям тут кешировать для первичного получения данных
+    // TODO can be cached
     const value = ref<T>(defaultValue)
 
-    // Получаем обновления данных из firebase
+    // Getting updates from firebase
     onValue(query, (snapshot) => {
-        const data: T = snapshot.val()
-        value.value = data
+        value.value = snapshot.val() as UnwrapRef<T>
     })
 
-    // Ключ все равно только на чтение, но может когда-нибудь можно будет использовать для админки
-    // // Отправляем обновления данных в firebase
+    // Without write access this code will only throw errors and send useless requests to firebase
+    // // Sending updates to firebase
     // watch(value, () => {
     //     set(query, value.value)
     // })
