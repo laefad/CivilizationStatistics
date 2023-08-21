77b472d4777232b153bbd4c28157f72dcf845774
TinyClayMan <70881505+TinyClayMan@users.noreply.github.com>
2023-08-21 13:10:04 +0300

    Fix: wrong link

--- pages/minecraft/about.vue
+++ pages/minecraft/about.vue
@@ -2,7 +2,7 @@
 
 import type { ServerInfo } from '@/types'
 
-const { data, pending, refresh } = await useLazyFetch<ServerInfo>('https://api.mcstatus.io/v2/status/java/pootgard.fun')
+const { data, pending, refresh } = await useLazyFetch<ServerInfo>('https://api.mcstatus.io/v2/status/java/dawgos.fun')
 
 onMounted(() => {
   setInterval(refresh, 60 * 1000)
